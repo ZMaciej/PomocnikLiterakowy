98e07033dfc4ad3d26274558cc8e1686e3823868
Maciej Zielonka <30180440+ZMaciej@users.noreply.github.com>
2026-04-16 23:06:12 +0200

    feat: revert game of day duration to 5 mins

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 let useMockMode = location.search.includes('mock');
 let useKidsMode = location.search.includes('kids');
-const GAME_OF_DAY_DURATION_SECONDS = 2; // 5 minutes
+const GAME_OF_DAY_DURATION_SECONDS = 5 * 60; // 5 minutes
 
 // ------------------------------------------------------------------------
 
